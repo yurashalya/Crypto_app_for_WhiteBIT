a8c1d8167f1ded669ca1c04fa6267da1c22cfc4a
yurashalya <stallker9696@gmail.com>
2024-07-11 08:07:41 +0300

    deleted unnecessary folders

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="bg-orange-500 flex-1 justify-center items-center">
+      <Text className="text-white text-3xl font-semibold">WhiteBIT</Text>
+    </View>
+  );
+};
+
+export default App;
